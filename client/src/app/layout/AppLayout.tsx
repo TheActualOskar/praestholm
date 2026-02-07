45906ba93b72d1adaf55934feffc9527e0309a0f
Oskar Præstholm <oskar.svendborg@gmail.com>
2026-02-07 14:18:01 +0100

    adding scroll fix

--- client/src/app/layout/AppLayout.tsx
+++ client/src/app/layout/AppLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import AnimatedBackground from "./AnimatedBackground";
+import ScrollToTop from "./ScrollToTop";
 
 export default function AppLayout() {
     return (
         <div className="app-shell">
             <AnimatedBackground />
+            <ScrollToTop />
             <Header />
             <main style={{ flex: 1 }}>
                 <Outlet />
